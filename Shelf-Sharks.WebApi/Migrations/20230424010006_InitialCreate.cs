cb3f979c3e7b7b4a6bef56cd278f0461965ce1f9
Nathan Laha <nlaha@outlook.com>
2023-04-23 18:05:22 -0700

    initialize db, fix image links

---
+++ Shelf-Sharks.WebApi/Migrations/20230424010006_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shelf_Sharks.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Author = table.Column<string>(type: "TEXT", nullable: true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    ISBN = table.Column<long>(type: "INTEGER", nullable: false),
+                    IsCheckedOut = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CoverURL = table.Column<string>(type: "TEXT", nullable: true),
+                    UUID = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DateAdded = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateCheckedOut = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateReturnBy = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
