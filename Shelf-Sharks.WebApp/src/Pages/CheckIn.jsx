23f4e47002eede4f744c2778889f4bd1a2146d6a
AdamKaraki <reebal.karaki28@gmail.com>
2023-04-28 19:45:06 -0700

    fixed checkIn and checkOut pages

--- Shelf-Sharks.WebApp/src/Pages/CheckIn.jsx
+++ Shelf-Sharks.WebApp/src/Pages/CheckIn.jsx
@@ -1,21 +1,54 @@
-import { Stack, Title, Flex, Group, ActionIcon } from "@mantine/core";
-import { IconArrowBack } from "@tabler/icons";
+import { Stack, Title, Flex } from "@mantine/core";
 import BookCard from "../Books/BookCard";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CheckIn(props) {
-  const navigate = useNavigate();
-  const goBack = () => {
-    navigate(-1);
+  const [books, setBooks] = useState([]);
+  const [returnedBooks, setReturnedBooks] = useState([]);
+
+  const apiURL = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    axios.get(`${apiURL}/books`).then((response) => {
+      setBooks(response.data);
+    });
+  }, [returnedBooks]);
+
+  const onCheckIn = (book) => {
+    // set book as checked in
+    book.isCheckedOut = false;
+
+    // update book in database
+    axios.put(`${apiURL}/books/${book.uuid}`, book);
+
+    // add book to returned books array
+    setReturnedBooks([...returnedBooks, book]);
+    
+    // reload page
+    window.location.reload();
   };
+
+  // Filter checked-out books
+  const checkedOutBooks = books.filter((book) => {
+    return book.isCheckedOut && !returnedBooks.includes(book);
+  });
+
   return (
     <Stack>
-      <Group>
-        <ActionIcon onClick={goBack}>
-          <IconArrowBack />
-        </ActionIcon>
-        <Title order={3}>Check In Books</Title>
-      </Group>
+      <Title order={3} mb="md">
+        Checked Out Books
+      </Title>
+      <Flex
+        miw={200}
+        direction={{ base: "column", sm: "row" }}
+        gap={{ base: "sm", sm: "lg" }}
+        wrap="wrap"
+      >
+        {checkedOutBooks.map((book) => {
+          return <BookCard book={book} onCheckIn={onCheckIn} />;
+        })}
+      </Flex>
     </Stack>
   );
 }
--- Shelf-Sharks.WebApp/src/Pages/CheckOut.jsx
+++ Shelf-Sharks.WebApp/src/Pages/CheckOut.jsx
@@ -1,21 +1,75 @@
-import { Stack, Title, Flex, Group, ActionIcon } from "@mantine/core";
-import { IconArrowBack } from "@tabler/icons";
+import { Stack, Title, Flex } from "@mantine/core";
 import BookCard from "../Books/BookCard";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CheckOut(props) {
-  const navigate = useNavigate();
-  const goBack = () => {
-    navigate(-1);
+  const [books, setBooks] = useState([]);
+
+  const apiURL = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    axios.get(`${apiURL}/books`).then((response) => {
+      setBooks(response.data);
+    });
+  }, []);
+
+  const onCheckOut = (book) => {
+    // set book as checked in
+    book.isCheckedOut = false;
+
+    // update book in books array
+    setBooks(
+      books.map((existingBook) => {
+        if (existingBook.uuid === book.uuid) {
+          return book;
+        }
+        return existingBook;
+      })
+    );
   };
+
+  const onCheckout = (book) => {
+    // set book as checked out
+    book.isCheckedOut = true;
+
+    // update book in books array
+    setBooks(
+      books.map((existingBook) => {
+        if (existingBook.uuid === book.uuid) {
+          return book;
+        }
+        return existingBook;
+      })
+    );
+  };
+
+  // Filter out checked-out books
+  const availableBooks = books.filter((book) => {
+    return !book.isCheckedOut;
+  });
+
   return (
     <Stack>
-      <Group>
-        <ActionIcon onClick={goBack}>
-          <IconArrowBack />
-        </ActionIcon>
-        <Title order={3}>Check Out Books</Title>
-      </Group>
+      <Title order={3} mb="md">
+        Available Books
+      </Title>
+      <Flex
+        miw={200}
+        direction={{ base: "column", sm: "row" }}
+        gap={{ base: "sm", sm: "lg" }}
+        wrap="wrap"
+      >
+        {availableBooks.map((book) => {
+          return (
+            <BookCard
+              book={book}
+              onCheckOut={onCheckOut}
+              onCheckout={onCheckout}
+            />
+          );
+        })}
+      </Flex>
     </Stack>
   );
 }
